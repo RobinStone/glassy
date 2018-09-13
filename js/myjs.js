73780559404ee6ad6732f999896706dc0b843780
RobinStone <vohuanrok@mail.ru>
2018-09-13 15:52:27 +0300

    created only not popap script

--- js/myjs.js
+++ js/myjs.js
@@ -21,32 +21,34 @@ $('.slider-btn').on('click', function(){
 
 //---------------------- MAP ---------------------------
 
-//ymaps.ready(init);
-//
-//function init() {
-//    var myMap = new ymaps.Map("map", {
-//        center: [59.938631, 30.323055],
-//        zoom: 17
-//    });
-//
-//    var collectionLabels = new ymaps.GeoObjectCollection({}, {
-//        iconLayout: 'default#image',
-//        iconImageHref: 'img/map-marker.png',
-//        iconImageSize: [40, 50],
-//        iconImageOffset: [-28, -40]
-//});
-//
-//    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-////        balloonContentHeader: 'ROBIN',
-////        balloonContentBody: 'ул. Мицеквича д.122 кв.10',
-////        balloonContentFooter: 'Первый подъезд, третий этаж',
-////        hintContent: 'Я ЖИВУ ТУТ'
-//    });
-//
-//    collectionLabels.add(myPlacemark);
-//    myMap.geoObjects.add(collectionLabels);
-//
-//}
+$('.static-map').addClass('visually-hidden');
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [59.938631, 30.323055],
+        zoom: 17
+    });
+
+    var collectionLabels = new ymaps.GeoObjectCollection({}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/marker.png',
+        iconImageSize: [170, 120],
+        iconImageOffset: [-55, -115]
+});
+
+    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+//        balloonContentHeader: 'ROBIN',
+//        balloonContentBody: 'ул. Мицеквича д.122 кв.10',
+//        balloonContentFooter: 'Первый подъезд, третий этаж',
+//        hintContent: 'Я ЖИВУ ТУТ'
+    });
+
+    collectionLabels.add(myPlacemark);
+    myMap.geoObjects.add(collectionLabels);
+
+}
 
 
 
